c1af9f5d8f3c881eac80563fdc81bfb908d5bd59
Hayden Smith <smithy21200@gmail.com>
2019-05-06 20:03:06 +0100

    Added second port option

--- src/com/lnicf/PortMiner/PortManager.java
+++ src/com/lnicf/PortMiner/PortManager.java
@@ -9,6 +9,7 @@ public class PortManager {
 	// Initialise PortMapping and UpnpService variables.
 	private static PortMapping mapping = null;
 	private static UpnpService upnpService = null;
+	private static UpnpService upnpServicePlugin = null;
 
 	public static void openPort(String internalIP, int port, String name, String protocol) {
 		// Create mapping, then use it.
@@ -22,9 +23,27 @@ public class PortManager {
 		upnpService = new UpnpServiceImpl(new PortMappingListener(mapping));
 		upnpService.getControlPoint().search();
 	}
+	
+	public static void openPortPlugin(String internalIP, int port, String name, String protocol) {
+		// Create mapping, then use it.
+		if (protocol == "TCP") {
+			mapping = new PortMapping(port, internalIP, PortMapping.Protocol.TCP, name);
+		} else if (protocol == "UDP") {
+			mapping = new PortMapping(port, internalIP, PortMapping.Protocol.UDP, name);
+		} else {
+			mapping = new PortMapping(port, internalIP, PortMapping.Protocol.TCP, name);
+		}
+		upnpServicePlugin = new UpnpServiceImpl(new PortMappingListener(mapping));
+		upnpServicePlugin.getControlPoint().search();
+	}
 
 	public static void closePort() {
 		// Stop the UPnP service, closing the port.
 		upnpService.shutdown();
+	}
+	
+	public static void closePortPlugin() {
+		// Stop the UPnP service, closing the port.
+		upnpServicePlugin.shutdown();
 	}
 }
